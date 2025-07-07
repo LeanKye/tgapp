db4b5491a9ed94c18925e7980048990b9de6dabe
LeanQ <leankye@users.noreply.github.com>
2025-07-08 01:01:19 +0300

    Улучшение дизайна и исправление багов

--- src/telegram-webapp.js
+++ src/telegram-webapp.js
@@ -1,6 +1,8 @@
 // Telegram Web App базовые настройки
 class TelegramWebApp {
   constructor() {
+    this.tg = null;
+    this.currentPage = this.detectCurrentPage();
     this.init();
   }
 
@@ -18,15 +20,19 @@ class TelegramWebApp {
   }
 
   setupTelegramWebApp() {
-    const tg = window.Telegram.WebApp;
+    this.tg = window.Telegram.WebApp;
     
     // Базовые настройки
-    tg.expand();
-    tg.disableVerticalSwipes();
-    tg.setHeaderColor('#000000');
+    this.tg.ready();
+    this.tg.expand();
+    this.tg.disableVerticalSwipes();
+    this.tg.setHeaderColor('#000000');
 
     // Настройки UI
     this.setupUIBehavior();
+    
+    // Управление кнопками
+    this.updateTelegramHeader();
   }
   
   // Настройка поведения UI
@@ -48,6 +54,52 @@ class TelegramWebApp {
       }
     });
   }
+
+  // Определение текущей страницы по URL
+  detectCurrentPage() {
+    const path = window.location.pathname;
+    const params = new URLSearchParams(window.location.search);
+    
+    if (path.includes('category.html')) {
+      return 'categories';
+    } else if (path.includes('product.html')) {
+      return 'product';
+    } else {
+      return 'home';
+    }
+  }
+
+
+
+  // Управление кнопками Telegram
+  updateTelegramHeader() {
+    if (!this.tg) return;
+
+    if (this.currentPage === "home") {
+      // На главной странице показываем кнопку "Закрыть"
+      this.tg.BackButton.hide();
+      this.tg.MainButton.setText("Закрыть");
+      this.tg.MainButton.show();
+      
+      // Удаляем предыдущие обработчики
+      this.tg.MainButton.offClick();
+      this.tg.MainButton.onClick(() => {
+        this.tg.close();
+      });
+    } else {
+      // На остальных страницах показываем кнопку "Назад"
+      this.tg.MainButton.hide();
+      this.tg.BackButton.show();
+      
+      // Удаляем предыдущие обработчики
+      this.tg.BackButton.offClick();
+      this.tg.BackButton.onClick(() => {
+        window.location.href = './';
+      });
+    }
+  }
+
+
 }
 
 // Глобальная переменная для доступа к экземпляру
